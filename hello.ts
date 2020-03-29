c4d47fe628521d236bf4c8c440e7eff2a76752c5
Stephen Cornelius <scornelius@gmail.com>
2020-03-29 12:52:34 +0100

    SC - extended example

--- hello.ts
+++ hello.ts
@@ -1,2 +1,19 @@
-let message : string = "Hello world";
-console.log(message);
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string
+    lastName: string
+}
+
+function greeter(person: Person) {
+    return "Hello " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane","M","User")
+
+console.log(greeter(user));
