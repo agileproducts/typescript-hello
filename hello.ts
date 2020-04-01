bc59517a5a42d3bb0040ba1290465746a4b31e71
Stephen Cornelius <scornelius@gmail.com>
2020-04-01 13:00:54 +0100

    SC - set up testing

--- hello.ts
+++ hello.ts
@@ -1,19 +1,5 @@
-class Student {
-    fullName: string;
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
-}
+import * as hellomod from './lib/hellomod';
 
-interface Person {
-    firstName: string
-    lastName: string
-}
+let user = new hellomod.Student("Jane","M","User")
 
-function greeter(person: Person) {
-    return "Hello " + person.firstName + " " + person.lastName;
-}
-
-let user = new Student("Jane","M","User")
-
-console.log(greeter(user));
+console.log(hellomod.greeter(user));
---
+++ test/hellomod.test.ts
@@ -0,0 +1,11 @@
+import 'mocha';
+import {assert} from 'chai';
+import * as hellomod from '../lib/hellomod';
+
+describe("hellomod", () => {
+    it("should return a greeting", (done) => {
+        let qa = new hellomod.Student("Joe","The","Tester")
+        assert.equal(hellomod.greeter(qa),"Hello Joe Tester");
+        done();
+    });
+});
