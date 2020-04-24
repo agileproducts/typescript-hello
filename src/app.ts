bc8aa951672c9ac0211635344674255984964552
Stephen Cornelius <scornelius@gmail.com>
2020-04-24 12:45:06 +0100

    SC - added form

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,20 @@ const app = express();
 
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", 'ejs');
+app.use(express.urlencoded({extended:true}));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.status(200);
     res.render("home");
+});
+
+app.get("/form", (req, res) => {
+    res.status(200);
+    res.render("form");
+});
+
+app.post("/form", (req,res) => {
+    res.status(200).json(req.body);
 });
 
 const port = process.env.PORT || 3000;
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,8 +1,32 @@
 import request from "supertest";
+import {assert} from "chai";
 import app from "../src/app";
 
 describe("GET /", () => {
     it("should return 200", (done) => {
         request(app).get("/").expect(200, done);
+    });
+});
+
+describe("GET /form", () => {
+    it("should return 200", (done) => {
+        request(app)
+            .get("/form")
+            .expect((res) => {
+                assert.include(res.text, "Form");
+            })
+            .expect(200,done);
+    });
+});
+
+describe("POST /form", () => {
+    it("should echo the payload sent", (done) => {
+        request(app)
+            .post("/form")
+            .send("email=bob@example.com")
+            .expect((res) => {
+                assert.equal(res.body.email, "bob@example.com");
+            })
+            .expect(200,done);
     });
 });
